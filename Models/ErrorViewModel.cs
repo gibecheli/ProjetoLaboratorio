12f20c99704a143ac22333e5968f807ddab57349
Giovana Becheli <98911613+gibecheli@users.noreply.github.com>
2023-07-16 13:28:14 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoLaboratorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
